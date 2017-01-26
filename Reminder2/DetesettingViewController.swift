827ce74df7982cc37983fec0c49f4f49dd1979e1
misako shirota <Misako@s-misako-shirotano-macbook-pro.local>
2017-01-26 19:47:06 +0900

    前のプロジェクトからコピペした　

---
+++ Reminder2/DetesettingViewController.swift
@@ -0,0 +1,76 @@
+//
+//  DatesettingViewController.swift
+//  reminder
+//
+//  Created by misako shirota on 2016/11/10.
+//  Copyright © 2016年 misako shirota. All rights reserved.
+//
+
+import UIKit
+
+import Social
+
+class DatesettingViewController: UIViewController {
+    
+    var remindText: String = ""
+    var dateText: String = ""
+    
+    
+    @IBOutlet var datePicker: UIDatePicker!
+    let now = NSDate()
+    
+    
+    @IBAction func go(sender :UIDatePicker) {
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "ja_JP") // ロケールの設定
+        dateFormatter.dateFormat = "yyyy年MM月dd日 HH:mm:ss" // 日付フォーマットの設定
+        
+        print(dateFormatter.stringFromDate(datePicker.date)) // -> 2014/06/25 02:13:18
+        dateText = dateFormatter.stringFromDate(datePicker.date)
+        
+        
+        print(datePicker.date)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        datePicker.date = now
+        
+        // Do any additional setup after loading the view.
+        
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "ja_JP") // ロケールの設定
+        dateFormatter.dateFormat = "yyyy年MM月dd日 HH:mm:ss" // 日付フォーマットの設定
+        
+        print(now)
+        print(dateFormatter.stringFromDate(datePicker.date)) // -> 2014/06/25 02:13:18
+        
+        print(remindText)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func twitterBtn(sender: AnyObject) {
+        var twitterVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        twitterVC.setInitialText(remindText+"を"+dateText+"までにやる！！")
+        presentViewController(twitterVC,animated: true, completion: nil)
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ Reminder2/ListViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ListViewController.swift
+//  reminder
+//
+//  Created by misako shirota on 2016/11/10.
+//  Copyright © 2016年 misako shirota. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var table: UITableView!
+    
+    //題名？を入れるための配列
+    var riminderNameArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //テーブルビューのデータベースメソッドはViewControllerクラスに書くよ、トイう設定
+        table.dataSource = self
+        //テーブルビューのデリケートメソッドはViewControllerクラスに書くよという設定
+        table.delegate = self
+        
+        //riminderNameArrayに題名を入れていく
+        riminderNameArray = ["う","え","おおおお"]
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //セルの数を設定する
+    func tableView(tableView: UITableView,numberOfRowsInSection section: Int) -> Int {
+        //セルの数をriminderNameArrayの要素の数にする
+        return riminderNameArray.count
+    }
+    
+    //IDつきのセルを取得してセル付属のtextLabelに「テスト」と表示させてみる
+    func tableView(tableView: UITableView,cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        
+        //セルにriminderNameArrayの題名を表示する
+        cell?.textLabel?.text = riminderNameArray[indexPath.row]
+        cell?.textLabel?.text = "テスト"
+        
+        return cell!
+    }
+    
+    //cellが押されたときの呼ばれるメソッド
+    func tableView(tableView: UITableView,didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        NSLog("%@が選ばれました",riminderNameArray[indexPath.row])
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
